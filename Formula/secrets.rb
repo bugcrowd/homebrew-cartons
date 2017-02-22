cfec44b452d57c983469aa8f9974ff41ccd6220f
Coen Hyde <coen.hyde@gmail.com>
2017-02-21 23:37:46 -0800

    updating secrets version

--- Formula/secrets.rb
+++ Formula/secrets.rb
@@ -2,8 +2,8 @@ class Secrets < Formula
   desc "Manage project secrets with Keybase encryption within a team"
   homepage "https://github.com/coen-hyde/secrets"
   url "https://github.com/coen-hyde/secrets/releases/download/alpha-rc1/secrets-darwin-amd64"
-  sha256 "384cba82c5da0bd68de64de9291bad682a61696aecd81239704c0da2d3bac59a"
-  version "alpha-rc1"
+  sha256 "30ba4ab79a25ae2275457c7aa676e513b252434779fe029715f5536ae7dffe0c"
+  version "v0.1.0"
 
   def install
     system "mv", "secrets-darwin-amd64", "secrets"

d5a7f31a059f6088fe6a8b0f8876779dab846068
Coen Hyde <coen.hyde@gmail.com>
2017-02-21 23:39:10 -0800

    fixing bug in secrets new release

--- Formula/secrets.rb
+++ Formula/secrets.rb
@@ -1,7 +1,7 @@
 class Secrets < Formula
   desc "Manage project secrets with Keybase encryption within a team"
   homepage "https://github.com/coen-hyde/secrets"
-  url "https://github.com/coen-hyde/secrets/releases/download/alpha-rc1/secrets-darwin-amd64"
+  url "https://github.com/coen-hyde/secrets/releases/download/v0.1.0/secrets-darwin-amd64"
   sha256 "30ba4ab79a25ae2275457c7aa676e513b252434779fe029715f5536ae7dffe0c"
   version "v0.1.0"
 

788cf7a46d99804a4cf0a1fdba418ffdab5dc8cb
Coen Hyde <coen.hyde@gmail.com>
2017-09-26 02:50:46 +0100

    updating secrest version to v0.2.0

--- Formula/secrets.rb
+++ Formula/secrets.rb
@@ -1,9 +1,9 @@
 class Secrets < Formula
   desc "Manage project secrets with Keybase encryption within a team"
   homepage "https://github.com/bugcrowd/secrets"
-  url "https://github.com/bugcrowd/secrets/releases/download/v0.1.0/secrets-darwin-amd64"
-  sha256 "30ba4ab79a25ae2275457c7aa676e513b252434779fe029715f5536ae7dffe0c"
-  version "v0.1.0"
+  url "https://github.com/bugcrowd/secrets/releases/download/v0.2.0/secrets-darwin-amd64"
+  sha256 "f8af9132bc6c268fe0312e3e1732018a8482af7089b09cc9f7e1b9f12d32732f"
+  version "v0.2.0"
 
   def install
     system "mv", "secrets-darwin-amd64", "secrets"
